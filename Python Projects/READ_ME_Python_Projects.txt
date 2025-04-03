456223f6afd91a4fcaffba531f57e517a4265785
jimmy-ho-projects <jimmyho1317@gmail.com>
2025-04-02 20:02:43 -0700

    Updated pseudo_ulam_spiral (v1.0 -> v1.2) python with prime generation and user query

--- Python Projects/READ_ME_Python_Projects.txt
+++ Python Projects/READ_ME_Python_Projects.txt
@@ -10,9 +10,10 @@ Contains projects made using python. Ran in VSCode.
     I don't think I'll go back organizing them so
     uuuhhh... this is the trash dump :3
 
-:_Pseudo_Ulam_Spiral.py
+:_Pseudo_Ulam_Spiral_v1.2.py
     Python port of modified ulam spiral. Number line originating from 0,0 will
     proceed towards right 1 unit at a time. When a prime number of units is 
     reached, the line direction will rotate 90 deg.
     TODO: Have graph update each iteration
     Created 4/1/25
+    v1.2 4/2/25
--- Python Projects/_Pseudo_Ulam_Spiral.py
+++ Python Projects/_Pseudo_Ulam_Spiral.py
@@ -3,55 +3,162 @@ Port of MATLAB Pseudo Ulam Spiral project
 Python port of modified ulam spiral. Number line originating from 0,0 will
 proceed towards right 1 unit at a time. When a prime number of units is 
 reached, the line direction will rotate 90 deg.
+NOTE: excluding 0 and 1 generates wildly different graph
 TODO: Have graph update each iteration
 Created 4/1/25
+
+Vers. 1.2 (4/2/25)
+    * Replaced fixed prime array with function to generate specified number of primes
+    * user-query stuff added
+    * option to include or exlude 0 and 1 from list of primes
+    * loading screen added
 - Jimmy Ho
 '''
 
 from time import sleep
 import matplotlib.pyplot as ploppy
 
-#excluding 0 and 1 generates wildly different graph
-primes = [0, 1, 2, 3,	5, 7, 11,	13,	17,	19,	23,	29, 31,	37,	41,	43,	47,	53,	59,	61,	67,	71,
-73,	79,	83,	89,	97,	101,	103,	107,	109,	113, 
-127,	131,	137,	139,	149,	151,	157,	163,	167,	173,
-179,	181,	191,	193,	197,	199,	211,	223,	227,	229, 
-233,	239,	241,	251,	257,	263,	269,	271,	277,	281, 
-283,	293,	307,	311,	313,	317,	331,	337,	347,	349, 
-353,	359,	367,	373,	379,	383,	389,	397,	401,	409, 
-419,	421,	431,	433,	439,	443,	449,	457,	461,	463, 
-467,	479,	487,	491,	499,	503,	509,	521,	523,	541, 
-547,	557,	563,	569,	571,	577,	587,	593,	599,	601, 
-607,	613,	617,	619,	631,	641,	643,	647,	653,	659, 
-661,	673,	677,	683,	691,	701,	709,	719,	727,	733, 
-739,	743,	751,	757,	761,	769,	773,	787,	797,	809, 
-811,	821,	823,	827,	829,	839,	853,	857,	859,	863, 
-877,	881,	883,	887,	907,	911,	919,	929,	937,	941, 
-947,	953,	967,	971,	977,	983,	991,	997,	1009,	1013, 
-1019,	1021,	1031,	1033,	1039,	1049,	1051,	1061,	1063,	1069, 
-1087,	1091,	1093,	1097,	1103,	1109,	1117,	1123,	1129,	1151, 
-1153,	1163,	1171,	1181,	1187,	1193,	1201,	1213,	1217,	1223, 
-1223,	1229,	1231,	1237,	1249,	1259,	1277,	1279,	1283,	1289, 
-1291,	1297,	1301,	1303,	1307,	1319,	1321,	1327,	1361,	1367, 
-1373,	1381,	1399,	1409,	1423,	1427,	1429,	1433,	1439,	1447, 
-1451,	1453,	1459,	1471,	1481,	1483,	1487,	1489,	1493,	1499, 
-1511,	1523,	1531,	1543,	1549,	1553,	1559,	1567,	1571,	1579, 
-1583,	1597,	1601,	1607,	1609,	1613,	1619,	1621,	1627,	1637, 
-1657,	1663,	1667,	1669,	1693,	1697,	1699,	1709,	1721,	1723, 
-1733,	1741,	1747,	1753,	1759,	1777,	1783,	1787,	1789,	1801, 
-1811,	1823,	1831,	1847,	1861,	1867,	1871,	1873,	1877,	1879, 
-1889,	1901,	1907,	1913,	1931,	1933,	1949,	1951,	1973,	1979, 
-1987,	1993,	1997,	1999,	2003,	2011,	2017,	2027,	2029,	2039, 
-2053,	2063,	2069,	2081,	2083,	2087,	2089,	2099,	2111,	2113, 
-2129,	2131,	2137,	2141,	2143,	2153,	2161,	2179,	2203,	2207, 
-2213,	2221,	2237,	2239,	2243,	2251,	2267,	2269,	2273,	2281, 
-2287,	2293,	2297,	2309,	2311,	2333,	2339,	2341,	2347,	2351, 
-2357,	2371,	2377,	2381,	2383,	2389,	2393,	2399,	2411,	2417, 
-2423,	2437,	2441,	2447,	2459,	2467,	2473,	2477,	2503,	2521, 
-2531,	2539,	2543,	2549,	2551,	2557,	2579,	2591,	2593,	2609, 
-2617,	2621,	2633,	2647,	2657,	2659,	2663,	2671,	2677,	2683, 
-2687,	2689,	2693,	2699,	2707,	2711,	2713,	2719,	2729,	2731]
+#====== functions ==========
+def loading_screen(currentIter, arrayPercentages):
+#ctrl+f DOGDOGDOGDOGDOGDOGwoofwoof
+    '''
+    _05 = arrayPercentages[0]
+    _10 = arrayPercentages[1]
+    _25 = arrayPercentages[2]
+    _50 = arrayPercentages[3]
+    _65 = arrayPercentages[4]
+    _75 = arrayPercentages[5]
+    _80 = arrayPercentages[6]
+    _90 = arrayPercentages[7]
+    _95 = arrayPercentages[8]
+    _100 = arrayPercentages[9]'''
+    if currentIter == 1:
+        print("[----------]....0%")
+    #account for range function jumping by 2s
+    if currentIter == int(arrayPercentages[9]) or currentIter == int(arrayPercentages[9]) - 1 or currentIter == int(arrayPercentages[9]) + 1:
+        print("[██████████]....100%")
+    try:
+        ind_of_percentage = arrayPercentages.index(currentIter)
+        match ind_of_percentage:
+            case 0:
+                print("[█--------].... 5%")
+            case 1:
+                print("[██--------]....10%")
+            case 2:
+                print("[███-------]....25%")
+            case 3:
+                print("[████------]....50%")
+            case 4:
+                print("[█████-----]....65%")
+            case 5:
+                print("[██████----]....75%")
+            case 6:
+                print("[███████---]....80%")
+            case 7:
+                print("[████████--]....90%")
+            case 8:
+                print("[█████████-]....95%")
+            case _:
+                ...
+    except:
+        ...
+            
+ 
+'''
+    match int(currentIter):
+        case int(_25):
+            print("cur " + str(currentIter))
+            print("per " + str(_25))
+        case int(_50):
+            print()
+        case int(_65):
+            print()
+        case int(_75):
+            print("FUCK")
+        case int(_80):
+            print()
+        case int(_90):
+            print()
+        case int(_95):
+            print("hella")
+'''
 
+#====== GENERATE PRIMES (pretty slow) ======
+primes = []
+loopVar = True
+while loopVar:
+    numPrimes = input("How many primes to find: ")
+    try:
+        numPrimes = int(numPrimes)
+        if numPrimes > 1000:
+            bloop = input("WARNING: Calculating more than 1000 primes will take some time. Are you sure?(Y/N) >")
+            if bloop.isalpha(): 
+                if bloop == 'y' or bloop == 'Y':
+                    loopVar = False
+                elif bloop == 'n' or bloop == 'N':
+                    loopVar = True
+                else:
+                    print("Invalid response")
+            else:
+                print("Invalid response")
+        else:
+            loopVar = False
+    except:
+        print("Please enter a whole number")
+
+'''
+# Check if every odd # up to numPrimes is a prime, 
+# inclusive range. Only need to check for factors 
+# less than half of value, rounded down, excluding 1
+'''
+
+#doing division once to pass to loading_screen funct
+#arbitrary chosen values, assume inverse exp computation
+#speed given number of inputs (DOGDOGDOGDOGDOGDOGwoofwoof)
+percentage_numPrimes = [int(numPrimes * .05), int(numPrimes * .1),
+                        int(numPrimes / 4), int(numPrimes / 2), 
+                        int(numPrimes * 0.65), int(numPrimes * 0.75), 
+                        int(numPrimes * 0.8), int(numPrimes * 0.90), 
+                        int(numPrimes * 0.95), numPrimes]
+#`````````````````````````````````````````````````````````````````````
+
+currVal = 0
+for slug in range(1, numPrimes+1, 2):
+    loading_screen(slug, percentage_numPrimes) #loading screen funct 
+    currVal = slug
+    notPrime = False
+    for factor_check in range(2, int(currVal/2)):
+        if currVal % factor_check == 0:
+            notPrime = True
+            break
+    if notPrime == False:
+        primes.append(currVal)        
+#2 is the only even prime, lil bugger
+primes.insert(1, int(2))
+# ================================================================
+
+
+#Query: Include 0 and 1? 
+# (Not considered primes but will alter graph without them)
+loopVar = True
+while loopVar:
+    include_zero_one = input("Include 0 and 1 with primes? (Y/N) >")
+    if include_zero_one.isalpha(): 
+        if include_zero_one == 'y' or include_zero_one == 'Y':
+            #1 already included, add 0
+            primes.insert(0, int(0))
+            loopVar = False
+        elif include_zero_one == 'n' or include_zero_one == 'n':
+            #1 already included, remove 1
+            primes.pop(0)
+            loopVar = False
+        else:
+            print("Invalid response, try again (Y/N) >")
+    
+
+print("Primes: " + str(primes))
+
+#Funny ulam spiral magic
 x=0 
 y=0 
 u=1 
@@ -74,11 +181,11 @@ for i in range(len(primes)): #iterate until reach value at last index of "primes
     if v == -1:
         y = y - 1
 
-    '''
-    checks if iteration is greater or equal to prime
-    if so, will increment to next prime on list and 
-    tell next vector to rotate 90 deg, counter-clockwise
-    '''
+
+#checks if iteration is greater or equal to prime
+#if so, will increment to next prime on list and 
+#tell next vector to rotate 90 deg, counter-clockwise
+
     
     if i >= primes[primeCounter]:
         primeCounter = primeCounter + 1
@@ -97,3 +204,4 @@ for i in range(len(primes)): #iterate until reach value at last index of "primes
 
     ploppy.plot([x, x+u],[y, y+v])
 ploppy.show()
+print("Program completed")
