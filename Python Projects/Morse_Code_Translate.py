f9e944e37d78908be4103146633521a447f2cba3
Jimmy Ho <jimmyho1317@gmail.com>
2025-05-19 00:43:43 -0700

    added inverse_morse dict

--- Python Projects/Morse_Code_Translate.py
+++ Python Projects/Morse_Code_Translate.py
@@ -1,35 +1,23 @@
 #Reference: https://students.cs.ucl.ac.uk/schoolslab/projects/PY2/introduction.html
 #https://morsecode.world/international/translator.html
 
+#dict MORSE -> LETTERS
 morse = {
-    ".-":"A" ,
-    "-...":"B" ,
-    "-.-.":"C" ,
-    "-..":"D" ,
-    ".":"E" ,
-    "..-.":"F" ,
-    "--.":"G" ,
-    "....":"H" ,
-    "..":"I" ,
-    ".---":"J" ,
-    "-.-":"K" ,
-    ".-..":"L" ,
-    "--":"M",
-    "-.":"N",
-    "---":"O",
-    ".--.":"P",
-    "--.-":"Q",
-    ".-.":"R",
-    "...":"S",
-    "-":"T",
-    "..-":"U",
-    "...-":"V",
-    ".--":"W",
-    "-..-":"X",
-    "-.--":"Y",
-    "--..":"Z",
-    "/":" " #space
+    ".-":"A" , "-...":"B" , "-.-.":"C" , "-..":"D" ,
+    ".":"E" , "..-.":"F" , "--.":"G" , "....":"H" ,
+    "..":"I" , ".---":"J" , "-.-":"K" , ".-..":"L" ,
+    "--":"M", "-.":"N", "---":"O", ".--.":"P",
+    "--.-":"Q", ".-.":"R", "...":"S", "-":"T",
+    "..-":"U", "...-":"V", ".--":"W", "-..-":"X",
+    "-.--":"Y", "--..":"Z", "/":" " #space
 }
+
+#dict LETTERS -> MORSE
+inverse_morse = {}
+for x in morse:
+    inverse_morse[morse[x]] = x
+
+print(inverse_morse)
 
 print("Enter Morse code to convert to text. Separate each character with a space. Use '/' to indicate space character in text.")
 #example input: .... . .-- .-- --- / .... .-- ..- -- .- -.
